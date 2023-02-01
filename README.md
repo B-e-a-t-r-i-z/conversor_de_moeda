9ae7728512affde8122dcae6cd61b87ef6bc3f58
Beatriz <beatriz.almeida2001@outlook.com>
2023-01-31 23:24:44 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# conversor_de_moeda
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/B-e-a-t-r-i-z/pen/poZxyWQ](https://codepen.io/B-e-a-t-r-i-z/pen/poZxyWQ).
+
+Um conversor de moedas, que converte um valor em Criptomoedas para Iene
---
+++ src/script.js
@@ -0,0 +1,8 @@
+var valorEmCriptomoeda = 30;
+var cotacaoEmCriptomoeda = 2994583.5;
+var valorEmIene = valorEmCriptomoeda * cotacaoEmCriptomoeda;
+var nome = "Beatriz";
+
+valorEmIene = valorEmIene.toFixed(2);
+
+alert("Olá " + nome + "! o valor em Iene é ¥ " + valorEmIene);
